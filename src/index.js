fe1d71fe04e33a1fab2c449a57a99d51f20b48ae
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-02-07 15:58:14 +0200

    corectloadmore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-  import api from "./api.js";
+import api from "./api.js";
 import LoadMoreBtn from "./loadMore.js";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
