8cef47d02b0f7671fc5277c1587eb9715cde1778
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-02-07 15:50:55 +0200

    cerection

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const API_KEY = "33338656-15c8519595d06b8db6a463933";
 
 
 
-async function getImage (query,page) {
+async function getImage (query, page) {
   const response = await axios.get(`${API_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
 
  return response.data.hits;
